b60c7bbb06a5b25224463a5d557d526438a440bb
Lucas Freixieiro <lucas@freixieiro.com>
2020-12-26 21:11:16 +0000

    Init HealthOrganization

---
+++ LEI_200221058_200221069/src/lei_200221058_200221069/HealthOrganizationMenu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lei_200221058_200221069;
+
+/**
+ *
+ * @author lukef
+ */
+public class HealthOrganizationMenu {
+    public void showHealtOrganizationMenu(){
+        System.out.println("\tSistema de rastreio de contactos em sala de aula");
+        System.out.println("\t\tÁrea da organização de Saúde");
+        System.out.println("Insira a sua opção:");
+        System.out.println("1 - Enviar lista de doentes infetados hoje");
+        System.out.println("2 - Ver Estatísticas");
+        System.out.println("0 - Sair");
+    }
+}
