e8e62f073cef3a45b7f5f4c8808c638de24e1248
Lucas Freixieiro <lucas@freixieiro.com>
2020-12-26 21:11:53 +0000

    Init Lesson

---
+++ LEI_200221058_200221069/src/lei_200221058_200221069/Lesson.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lei_200221058_200221069;
+
+/**
+ *
+ * @author Lucas Freixieiro
+ */
+
+/*
+Classe que contém as informações acerca de uma aula
+*/
+public class Lesson {
+    
+}
