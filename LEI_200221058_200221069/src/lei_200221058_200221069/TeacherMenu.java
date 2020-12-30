fee2ff144b492e8c80bcdd22610155af532548d9
Lucas Freixieiro <lucas@freixieiro.com>
2020-12-30 15:33:57 +0000

    Init TeacherMenu

---
+++ LEI_200221058_200221069/src/lei_200221058_200221069/TeacherMenu.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lei_200221058_200221069;
+
+/**
+ *
+ * @author lukef
+ */
+public class TeacherMenu {
+    public void showTeacherMenu(){
+       System.out.println("1 -Registar Presenças");
+       System.out.println("2 - Iniciar aula");
+       System.out.println("3 - Terminar aula");
+       System.out.println("0 - Sair");
+   }
+}
