87650efb7f72c10897c5f7c8da6f76ea42e61bb2
Lucas Freixieiro <lucas@freixieiro.com>
2021-01-03 00:37:50 +0000

    Valid numberID and new method for userState

--- LEI_200221058_200221069/src/lei_200221058_200221069/InputReader.java
+++ LEI_200221058_200221069/src/lei_200221058_200221069/InputReader.java
@@ -21,7 +21,7 @@ public class InputReader {
         reader = new Scanner(System.in);
     }
     
-    public int getNumber(String question) {
+    public int getOption(String question) {
         if(question == null) {
         question = "";
         }
@@ -36,6 +36,25 @@ public class InputReader {
         return number;
     }
     
+    public int getUserID(String question){
+        if(question == null) {
+        question = "";
+        }
+        question += "> ";
+        System.out.print(question);
+        while (!reader.hasNextInt()) {
+        reader.nextLine();
+        System.out.print(question);
+        }
+        int number = reader.nextInt();
+        while(((int) (Math.log10(number) + 1)) != 9){
+            System.out.print(question);
+            number = reader.nextInt();
+        }
+        reader.nextLine();
+        return number;        
+    }
+    
     public String getText(String question){
         if(question == null){
             question = "";
@@ -44,5 +63,46 @@ public class InputReader {
         System.out.print(question); 
         
         return reader.nextLine();
+    }
+    
+    public UserState getUserState(String question){
+        UserState status = UserState.NORMAL;
+        System.out.println("Estados:");
+        System.out.println("1 - Contínuo");
+        System.out.println("2 - Isolamento");
+        System.out.println("3 - Infetado");
+        if(question == null) {
+        question = "";
+        }
+        question += "> ";
+        System.out.print(question);
+        while (!reader.hasNextInt()) {
+        reader.nextLine();
+        System.out.print(question);
+        }
+        int option = reader.nextInt();
+        while(option > 3 || option < 1){
+            System.out.print(question);
+            option = reader.nextInt();
+        }
+        switch(option){
+            case 1:{
+                status = UserState.NORMAL;
+                break;
+            }
+            case 2:{
+                status = UserState.ISOLATION;
+                break;
+            }
+            case 3:{
+                status = UserState.INFECTED;
+                break;
+            }
+            default:{
+                System.out.println("Erro no Sistema! Opção não reconhecida");
+            }
+        }
+        reader.nextLine();
+        return status;
     }
 }
