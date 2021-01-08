89f151ad3de0353d1cacd4d496cc33f88b5bcefb
LillyTS <75341426+LillyTS@users.noreply.github.com>
2021-01-08 19:13:24 +0000

    remove useless methods

--- JavaProject1/Test Package Folder/lei_200221058_200221069/LessonTest.java
+++ JavaProject1/Test Package Folder/lei_200221058_200221069/LessonTest.java
@@ -29,22 +29,6 @@ public class LessonTest {
     public LessonTest() {
     }
 
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
     /**
      * Test of startLesson method, of class Lesson.
      */
