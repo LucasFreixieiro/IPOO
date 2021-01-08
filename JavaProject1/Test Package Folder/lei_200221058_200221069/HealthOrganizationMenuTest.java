0ebcc95604cdf171f55a85c0d73649c8d452e347
LillyTS <75341426+LillyTS@users.noreply.github.com>
2021-01-08 14:00:48 +0000

    Test Package Folder -> só criei o HealthMenuTest por defeito

---
+++ JavaProject1/Test Package Folder/lei_200221058_200221069/HealthOrganizationMenuTest.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lei_200221058_200221069;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lili
+ */
+public class HealthOrganizationMenuTest {
+    
+    public HealthOrganizationMenuTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of run method, of class HealthOrganizationMenu.
+     */
+    @Test
+    public void testRun() {
+        System.out.println("run");
+        HealthOrganizationMenu instance = null;
+        instance.run();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of showHealthOrganizationMenu method, of class HealthOrganizationMenu.
+     */
+    @Test
+    public void testShowHealthOrganizationMenu() {
+        System.out.println("showHealthOrganizationMenu");
+        HealthOrganizationMenu instance = null;
+        instance.showHealthOrganizationMenu();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removeRecomendation method, of class HealthOrganizationMenu.
+     */
+    @Test
+    public void testRemoveRecomendation() {
+        System.out.println("removeRecomendation");
+        HealthOrganizationMenu instance = null;
+        instance.removeRecomendation();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of editRecomendation method, of class HealthOrganizationMenu.
+     */
+    @Test
+    public void testEditRecomendation() {
+        System.out.println("editRecomendation");
+        HealthOrganizationMenu instance = null;
+        instance.editRecomendation();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of sendList method, of class HealthOrganizationMenu.
+     */
+    @Test
+    public void testSendList() {
+        System.out.println("sendList");
+        HealthOrganizationMenu instance = null;
+        instance.sendList();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
