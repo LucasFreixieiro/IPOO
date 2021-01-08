1adb026a277e55e0c08cc367c23fffe3ff9bc3bd
LillyTS <75341426+LillyTS@users.noreply.github.com>
2021-01-08 14:10:00 +0000

    Test Package Folder ->  Default Menu and AppStart creation not working yet

---
+++ JavaProject1/Test Package Folder/lei_200221058_200221069/AppStartTest.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lei_200221058_200221069;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lili
+ */
+public class AppStartTest {
+    
+    public AppStartTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class AppStart.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        AppStart.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of showMainMenu method, of class AppStart.
+     */
+    @Test
+    public void testShowMainMenu() {
+        System.out.println("showMainMenu");
+        AppStart.showMainMenu();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
