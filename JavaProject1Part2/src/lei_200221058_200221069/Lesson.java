f1ff751fc6822cebe0d2e9fe6b98dbd6192acb25
lukeft <36772121+lukeft@users.noreply.github.com>
2021-01-25 19:41:43 +0000

    Ids nas presenças fix

--- JavaProject1Part2/src/lei_200221058_200221069/Lesson.java
+++ JavaProject1Part2/src/lei_200221058_200221069/Lesson.java
@@ -105,7 +105,7 @@ public class Lesson {
         this.attendancesAtStart = attendancesAtStart;
         lessonStart = LocalTime.now();
         lessonMapStart = map;
-        this.attendancesAtStart.setIDs();
+        setIDAtStart();
     }
 
     /**
@@ -123,7 +123,25 @@ public class Lesson {
 
         this.attendancesAtEnd = attendancesAtEnd;
         lessonEnd = LocalTime.now();
-        this.attendancesAtEnd.setIDs();
+        this.lessonMapEnd = map;
+        setIDAtEnd();
+        ArrayList<User> users1 = this.attendancesAtEnd.getUsers();
+        for(User user1 : users1){
+            System.out.println(user1.getUserID());
+            System.out.println("Transmitidos:");
+            ArrayList<Id> ids = user1.getTransmitedIds();
+            for (Id id : ids) {
+                System.out.println(id.toString());
+            }
+            System.out.println("----------");
+            System.out.println("Recebidos:");
+            ArrayList<Id> ids2 = user1.getReceivedIDs();
+            for (Id id : ids2) {
+                System.out.println(id.toString());
+            }
+            System.out.println("----------");
+        }
+        //this.attendancesAtEnd.setIDsAtEnd();
     }
 
     /**
@@ -176,38 +194,141 @@ public class Lesson {
         User user2;
         for(int i=0; i<lessonMapStart.length; i++){
             for(int j=0; j<lessonMapStart[i].length; j++){
-                user = getUser(lessonMapStart[i][j], 1);
-                if(user != null){
-                    ID = UUID.randomUUID().toString();
-                    today = LocalDate.now();
-                    user.setTransmitedIds(ID, today);
-                    //Repetir a repetição do array
-                    //Serve para enviar o id parra os alunos na sala de aula
-                    for(int z=0; z<lessonMapStart.length; z++){
-                        for(int x=0; x<lessonMapStart[z].length; x++){
-                            if(z != i && x != j){
-                                user2 = getUser(lessonMapStart[z][x], 1);
-                                if(i==0){
-                                    if(j==0){
-                                        if(z == i+1 && x == j+1){
+                if(lessonMapStart[i][j] != 0){
+                    user = getUser(lessonMapStart[i][j], 1);
+                    if(user != null){
+                        ID = UUID.randomUUID().toString();
+                        today = LocalDate.now();
+                        user.setTransmitedIds(ID, today);
+                        System.out.println(ID);
+                        //Repetir a repetição do array
+                        //Serve para enviar o id parra os alunos na sala de aula
+                        for(int z=0; z<lessonMapStart.length; z++){
+                            for(int x=0; x<lessonMapStart[z].length; x++){
+                                if(lessonMapStart[z][x] != 0){
+                                    if(z != i || x != j){
+                                        System.out.println(lessonMapStart[z][x]);
+                                        user2 = getUser(lessonMapStart[z][x], 1);
+                                        if(isBelow(z, i) && x == j){
+                                            user2.setReceivedIDs(ID, today, 2);
+                                        }
+                                        else if(isUp(z, i) && x == j){
+                                            user2.setReceivedIDs(ID, today, 2);
+                                        }
+                                        else if(isLeft(x, j) && z == i){
+                                            user2.setReceivedIDs(ID, today, 2);
+                                        }
+                                        else if(isRight(x, j) && z == i){
+                                            user2.setReceivedIDs(ID, today, 2);
+                                        }
+                                        else if(isRight(x, j+1) && z == i){
                                             user2.setReceivedIDs(ID, today, 4);
                                         }
-                                        else
+                                        else if(isLeft(x, j-1) && z == i){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else if(isUp(z, i+1) && x == j){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else if(isBelow(z, i-1) && x == j){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else if(isLeft(x, j) && isUp(z, i)){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else if(isLeft(x, j) && isBelow(z, i)){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else if(isRight(x, j) && isUp(z, i)){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else if(isRight(x, j) && isBelow(z, i)){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else{
                                             user2.setReceivedIDs(ID, today, 5);
+                                        }
                                     }
-                                    else if(j<lessonMapStart[i].length - 1){
-                                        if(z == )
+                                }
+                            }
+                        }
+                    }    
+                }  
+            }
+        }
+    }
+
+        /**
+     * Define os Ids recebidos para cada utilizador.
+     * Utiliza os ids transmitidos para preencher o array dos ids recebidos
+     */
+    public void setIDAtEnd() {
+        String ID;
+        LocalDate today;
+        User user;
+        User user2;
+        for(int i=0; i<lessonMapEnd.length; i++){
+            for(int j=0; j<lessonMapEnd[i].length; j++){
+                if(lessonMapEnd[i][j] != 0){
+                    user = getUser(lessonMapEnd[i][j], 2);
+                    if(user != null){
+                        ID = UUID.randomUUID().toString();
+                        today = LocalDate.now();
+                        user.setTransmitedIds(ID, today);
+                        //Repetir a repetição do array
+                        //Serve para enviar o id parra os alunos na sala de aula
+                        for(int z=0; z<lessonMapEnd.length; z++){
+                            for(int x=0; x<lessonMapEnd[z].length; x++){
+                                if(lessonMapStart[z][x] != 0){
+                                    if(z != i || x != j){
+                                        System.out.println(lessonMapStart[z][x]);
+                                        user2 = getUser(lessonMapStart[z][x], 1);
+                                        if(isBelow(z, i) && x == j){
+                                            user2.setReceivedIDs(ID, today, 2);
+                                        }
+                                        else if(isUp(z, i) && x == j){
+                                            user2.setReceivedIDs(ID, today, 2);
+                                        }
+                                        else if(isLeft(x, j) && z == i){
+                                            user2.setReceivedIDs(ID, today, 2);
+                                        }
+                                        else if(isRight(x, j) && z == i){
+                                            user2.setReceivedIDs(ID, today, 2);
+                                        }
+                                        else if(isRight(x, j+1) && z == i){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else if(isLeft(x, j-1) && z == i){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else if(isUp(z, i+1) && x == j){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else if(isBelow(z, i-1) && x == j){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else if(isLeft(x, j) && isUp(z, i)){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else if(isLeft(x, j) && isBelow(z, i)){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else if(isRight(x, j) && isUp(z, i)){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else if(isRight(x, j) && isBelow(z, i)){
+                                            user2.setReceivedIDs(ID, today, 4);
+                                        }
+                                        else{
+                                            user2.setReceivedIDs(ID, today, 5);
+                                        }
                                     }
                                 }
                             }
                         }
                     }
                 }
-                
-
             }
-            
-            
         }
 
         /*String ID;
@@ -237,4 +358,30 @@ public class Lesson {
             return null;
         
     }
+
+    public boolean isBelow(int currentPosition, int mainPosition){
+        return currentPosition - mainPosition == 1;
+    }
+
+    public boolean isUp(int currentPosition, int mainPosition){
+        return mainPosition - currentPosition == 1;
+    }
+
+    public boolean isLeft(int currentPosition, int mainPosition){
+        return mainPosition - currentPosition == 1;
+    }
+
+    public boolean isRight(int currentPosition, int mainPosition){
+        return currentPosition - mainPosition == 1;
+    }
+
+    public void setAttendancesAtStart(UserDB attendancesAtStart) {
+        this.attendancesAtStart = attendancesAtStart;
+    }
+
+    public void setAttendancesAtEnd(UserDB attendancesAtEnd) {
+        this.attendancesAtEnd = attendancesAtEnd;
+    }
+
+    
 }
